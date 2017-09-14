a4917fadb6cb16e9341f384db5646d215c8d9e83
96qbhy <96qbhy@gmail.com>
2017-09-14 10:15:43 +0800

    更新包名，添加string.go

---
+++ String.go
@@ -0,0 +1,32 @@
+package utils
+
+// 截取字符串 start 起点下标 length 需要截取的长度
+func SubStr(str string, start int, length int) string {
+	rs := []rune(str)
+	rl := len(rs)
+	end := 0
+
+	if start < 0 {
+		start = rl - 1 + start
+	}
+	end = start + length
+
+	if start > end {
+		start, end = end, start
+	}
+
+	if start < 0 {
+		start = 0
+	}
+	if start > rl {
+		start = rl
+	}
+	if end < 0 {
+		end = 0
+	}
+	if end > rl {
+		end = rl
+	}
+
+	return string(rs[start:end])
+}
